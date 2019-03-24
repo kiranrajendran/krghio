532fd5d9242118e2713abc76f63dbccf97761f89
Kiran Rajendran <kiran.rajendran@gmail.com>
2019-03-24 11:49:44 -0400

    read dynamodb

--- dynamho/udjc.java
+++ dynamho/udjc.java
@@ -12,59 +12,62 @@ import com.amazonaws.services.dynamodbv2.document.ItemCollection;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
 import com.amazonaws.regions.*;
 
-public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException {
 
 
-    Object[] r = getRow();
-    if (r == null) {
-        setOutputDone();
-        return false;
+public boolean processRow(StepMetaInterface smi, StepDataInterface sdi) throws KettleException 
+{
+
+
+	Object[] r = getRow();
+	if (r == null) {
+		setOutputDone();
+		return false;
+	}
+
+
+  AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient(); //.build;
+	// .withRegion(Regions.US_WEST_2).build();
+  DynamoDB dynamoDB = new DynamoDB(client);
+
+  Table table = dynamoDB.getTable("servers");
+
+  String server_name = getInputRowMeta().getString(r, getParameter("SERVERNAME"), null );
+    
+  QuerySpec spec = new QuerySpec()
+    .withKeyConditionExpression("server_name = :v_server_name")
+    .withValueMap(new ValueMap()
+      .withString(":v_server_name", server_name));
+
+  ItemCollection<QueryOutcome> items = table.query(spec);
+
+  Iterator<Item> iterator = items.iterator();
+  Item item = null;
+  while (iterator.hasNext()) {
+      item = (Item) iterator.next();
+      logBasic(item.toJSONPretty());
     }
 
+  
+  r = createOutputRow(r, data.outputRowMeta.size());
 
-    AmazonDynamoDB client = AmazonDynamoDBClientBuilder.defaultClient(); //.build;
-    // .withRegion(Regions.US_WEST_2).build();
-    DynamoDB dynamoDB = new DynamoDB(client);
+  int index = getInputRowMeta().size();
+  
+  r[index++] = item == null ? null : item.getStringSet("server_name");
+  r[index++] = item == null ? null : item.getStringSet("DB_HOST");
+  r[index++] = item == null ? null : item.getStringSet("DB_PORT");
+  r[index++] = item == null ? null : item.getStringSet("DB_SCHEMA");
+  r[index++] = item == null ? null : item.getStringSet("DB_USER");
+  r[index++] = item == null ? null : item.getStringSet("DB_PASS");
+  
+  putRow(data.outputRowMeta, r); 
 
-    Table table = dynamoDB.getTable("servers");
-
-    String server_name = getInputRowMeta().getString(r, getParameter("SERVERNAME"), null);
-
-    QuerySpec spec = new QuerySpec()
-        .withKeyConditionExpression("server_name = :v_server_name")
-        .withValueMap(new ValueMap()
-            .withString(":v_server_name", server_name));
-
-    ItemCollection < QueryOutcome > items = table.query(spec);
-
-    Iterator < Item > iterator = items.iterator();
-    Item item = null;
-    while (iterator.hasNext()) {
-        item = (Item) iterator.next();
-        logBasic(item.toJSONPretty());
-    }
-
-
-    r = createOutputRow(r, data.outputRowMeta.size());
-
-    int index = getInputRowMeta().size();
-
-    r[index++] = item.getStringSet("server_name");;
-    r[index++] = item.getStringSet("DB_HOST");
-    r[index++] = item.getStringSet("DB_PORT");;
-    r[index++] = item.getStringSet("DB_SCHEMA");;
-    r[index++] = item.getStringSet("DB_USER");;
-    r[index++] = item.getStringSet("DB_PASS");;
-
-    putRow(data.outputRowMeta, r);
-
-    return true;
+   return true;
 }
 
 public boolean init(StepMetaInterface stepMetaInterface, StepDataInterface stepDataInterface) {
-    if (super.init(stepMetaInterface, stepDataInterface)) {
+   if (super.init(stepMetaInterface, stepDataInterface)) {
 
-        return true;
-    }
-    return false;
+     return true;
+   }
+   return false;
 }
